170e309b3f8d06c0310ee7fcf3289b81134d6e11
Angela-Rupay <u20242227383@usco.edu.co>
2026-09-03 13:40:36 -0500

    first commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,25 @@
+import styled from "@emotion/styled"
+
+const InputSubmit = styled.input`
+  background-color: #9497FF;
+  border: none;
+  width: 100%;
+  padding: 10px;
+  color: #ffffff;
+  font-weight: 700;
+  text-transform: uppercase;
+  font-size: 20px;
+  border-radius: 5px;
+`
+const Formulario = () => {
+  return (
+    <form>
+        <InputSubmit
+        type="submit"
+        value="Cotizar"
+        />
+    </form>
+  )
+}
+
+export default Formulario
